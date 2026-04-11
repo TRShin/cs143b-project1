0b640807c39882a9684d61b22622b2ec0a1a8867
TRShin <trentys@yahoo.com>
2026-04-11 15:05:17 -0700

    did stuff create func

--- src/main.cpp
+++ src/main.cpp
@@ -15,26 +15,45 @@ int main(int argc, char* argv[])
 
 		stringstream args(line);
 		string a1;
+		int val;
 		args >> a1;
 
 		if (a1 == "in") {
 			init();
 		}
+
 		else if (a1 == "cr") {
-			
-		} 
+			create();
+		}
+
 		else if (a1 == "de") {
-
+			int val;
+			if (args >> val) {
+				destroy(val)
+			}
+			else continue;
 		}
+
 		else if (a1 == "rq") {
-
+			int val;
+			if (args >> val) {
+				request(val)
+			}
+			else continue;
 		}
+
 		else if (a1 == "rl") {
+			int val;
+			if (args >> val) {
+				release(val);
+			}
+			else continue;
+		}
 
+		else if (a1 == "to") {
+			timeout();
 		}
-		else if (a1 == (a1 == "to") {
-		}
-		else {}
+	}
 
 	return 0;
 }
--- src/operations.cpp
+++ src/operations.cpp
@@ -1,12 +1,42 @@
 #include "operations.h"
 
+using namespace std;
+
 // Declare all DS
 process PCB[16];
 resource RCB[4];
 list<int> RL;
 
 // Operations on Processes
-void create() {}
+void create()
+{
+	int index;
+
+	// Find next process to be created
+	auto it = find_if(begin(PCB), end(PCB), [](const auto& p) {
+		return p.state == false;
+		});
+	if (it != end(PCB)) {
+		index = it - begin(PCB);
+	}
+	else {
+		cout << "error" << endl;
+		return;
+	}
+
+	// Check who the parent is, update parent
+	int parent_index = RL.front();
+	PCB[parent_index].children.push_back(index);
+
+	// Allocate new pcb[index]
+	PCB[index] = { true, parent_index, {}, {} };
+
+	// Push back onto RL
+	RL.push_back(index);
+
+	cout << "Process " << index << " created" << endl;
+}
+
 void destroy(process j) {}
 
 // Operations on Resources
@@ -27,7 +57,7 @@ void init()
 	 * false = blocked, true = ready
 	 */
 	PCB[0] = { false, 0, {}, {} }
-	for (int i = 1; i < 16 - 1; i++) {
+	for (int i = 1; i < 15; i++) {
 		PCB[i] = { false, 0, {}, {} };
 	}
 
@@ -38,7 +68,7 @@ void init()
 	 * Resource Control Block Init
 	 * false = free
 	 */
-	for (int i = 0; i < 4 - 1; i++) {
+	for (int i = 0; i < 3; i++) {
 		RCB[i] = { false, {} };
 	}
 }
